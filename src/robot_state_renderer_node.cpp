3231fc32218669c46609dde9e809eae9d8dc1490
Christian Rauch <Christian.Rauch@ed.ac.uk>
2019-01-16 19:17:22 +0000

    fix parsing of URDF path from program arguments

--- src/robot_state_renderer_node.cpp
+++ src/robot_state_renderer_node.cpp
@@ -2,8 +2,9 @@
 
 int main(int argc, char *argv[]) {
     ros::init(argc, argv, "StateRenderer");
+    const std::string urdf_path = (argc > 1) ? std::string(argv[1]) : std::string();
     if(ros::param::param("~visualise", true))
-        StateRenderer(std::to_string(*argv[1])).visualise();
+        StateRenderer(urdf_path).visualise();
     else
-        StateRenderer(std::to_string(*argv[1])).spin();
+        StateRenderer(urdf_path).spin();
 }
